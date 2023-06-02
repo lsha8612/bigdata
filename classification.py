7b75e033ed191ae11697b09e117a30883531493e
lsha <lsha8612@naver.com>
2023-06-02 16:29:14 +0900

    commit

--- classification.py
+++ classification.py
@@ -5,4 +5,5 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv")
-print(df)
+
+##데이터 전처리 - 결측치 대체
